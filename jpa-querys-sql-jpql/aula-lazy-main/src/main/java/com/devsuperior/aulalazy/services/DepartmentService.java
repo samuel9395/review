b10fed3f60782929f8ecac3fca77bd34018cc77f
Samuel-Freitas-NAVA <samuel.freitas@nava.com.br>
2026-01-08 09:50:32 -0300

    Módulo JPA - Consultas SQL e JPQL

---
+++ jpa-querys-sql-jpql/aula-lazy-main/src/main/java/com/devsuperior/aulalazy/services/DepartmentService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.aulalazy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.aulalazy.dto.DepartmentDTO;
+import com.devsuperior.aulalazy.dto.EmployeeMinDTO;
+import com.devsuperior.aulalazy.entities.Department;
+import com.devsuperior.aulalazy.entities.Employee;
+import com.devsuperior.aulalazy.repositories.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+
+	@Autowired
+	private DepartmentRepository repository;
+
+	@Transactional(readOnly = true)
+	public DepartmentDTO findById(Long id) {
+		Optional<Department> result = repository.findById(id);
+		return new DepartmentDTO(result.get());
+	}
+
+	@Transactional(readOnly = true)
+	public List<EmployeeMinDTO> findEmployeesByDepartment(Long id) {
+		Optional<Department> result = repository.findById(id);
+		List<Employee> list = result.get().getEmployees();
+		return list.stream().map(x -> new EmployeeMinDTO(x)).toList();
+	}
+}
---
+++ jpa-querys-sql-jpql/aula-salvar-para-muitos-main/src/main/java/com/devsuperior/aula/dto/ProductDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.aula.dto;
+
+import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Aqui instanciamos o ProductDTO com as categorias aninhadas á ele
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+
+        for (Category category : entity.getCategories()) {
+            categories.add(new CategoryDTO(category));
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+}
