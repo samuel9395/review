94702226670360aaaf9e5d038bbd338231c2463a
Samuel-Freitas-NAVA <samuel.freitas@nava.com.br>
2026-01-16 11:59:24 -0300

    BCrypt password

--- login-controle-acesso/spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ login-controle-acesso/spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$gZB5I0ecFHTiifL8fCdjZeIA/UPp38ttSRVtUBhpvE0jY9LRzHzfu');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$gZB5I0ecFHTiifL8fCdjZeIA/UPp38ttSRVtUBhpvE0jY9LRzHzfu');
 
 -- O padrão 'ROLE_' é do spring security
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
