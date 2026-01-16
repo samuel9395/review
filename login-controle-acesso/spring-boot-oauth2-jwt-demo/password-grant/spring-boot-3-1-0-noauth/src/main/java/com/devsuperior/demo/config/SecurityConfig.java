7563c1f11e5efd17ad9acf32efdc31d855e3b4d7
Samuel-Freitas-NAVA <samuel.freitas@nava.com.br>
2026-01-16 10:38:31 -0300

    Spring Security

---
+++ login-controle-acesso/spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()); // Aqui acessa o objeto http do tipo HttpSecurity e chamando o csrf.disable, para desabilitar a proteção contra ataques do tipo csrf(dados da seção)
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Aqui chama outra configuração para configurar a permissão para os endpoints da aplicação
+        return http.build();
+    }
+
+}
