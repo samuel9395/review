3f78c9d4b633850e97e4b8e4e415545d73533e0f
Samuel Bitencourt <samuelb2f@outlook.com>
2026-01-19 17:19:55 -0300

    UserDetails, GrantedAuthority, not tested

--- modelo-dominio-orm/dscommerce/src/main/java/com/projectdevsuperior/dscommerce/entities/Role.java
+++ modelo-dominio-orm/dscommerce/src/main/java/com/projectdevsuperior/dscommerce/entities/Role.java
@@ -1,10 +1,12 @@
 package com.projectdevsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +28,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
